611cfe176212c86973ae079d7ce156a7dc566bf4
Ying Sun <home9464@gmail.com>
2026-01-08 16:41:32 -0800

    update

--- debug_scan.py
+++ debug_scan.py
@@ -9,7 +9,7 @@ def scan_inverter(instrument, address):
     try:
         instrument.address = address
         found_any = False
-        for reg in range(500, 600):
+        for reg in [261, 267, 269]:
             try:
                 val = instrument.read_register(reg, 0, functioncode=3)
                 if val > 0:
